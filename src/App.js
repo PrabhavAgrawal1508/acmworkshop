78b05b163620d41a5e8d9307144666fe9ed73377
PrabhavAgrawal1508 <75772633+PrabhavAgrawal1508@users.noreply.github.com>
2023-04-15 03:00:46 +0530

    After adding phone constraints

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
 import './App.css';
-import Home from './Components/Home'
+import Home from './Components/home';
 import Navbar from './Components/navbar';
-
+import About from './Components/About';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home/>
+      <About/>
 
     </div>
   );
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import logo from '../Assets/svg.ghost-orb-logo.png'
 import '../Styles/Navbar.css'
+import hamburger_website from '../Assets/Vector.png'
 function Navbar() {
-  return (
-    <div className = 'navbar_main_container'>
+    const [showNav, setShowNav] = React.useState(false)
+    return (
+        <div className='navbar_main_container'>
+            <div className='phone_container'>
+                <div className='logo_container'>
+                    <img src={logo} />
+                </div>
+                <img onClick={() => { setShowNav(!showNav) }} className='hamburger' src={hamburger_website} />
 
-        <div className='logo_container'>
-            <img src={logo}/>
+            </div>
+            <div className={'list_signup ' + (showNav && 'active')}>
+                <div className='list_container'>
+                    <p>Product</p>
+                    <p>Resource</p>
+                    <p>Pricing</p>
+                    <p>Explore</p>
+                </div>
+                <div className='signup_container'>
+                    <p>Sign Up</p>
+                </div>
+            </div>
         </div>
-        <div className='list_signup'>
-        <div className='list_container'>
-            <p>Product</p>
-            <p>Resource</p>
-            <p>Pricing</p>
-            <p>Explore</p>
-        </div>
-        <div className='signup_container'><p>Sign Up</p></div></div>
-
-
-
-
-
-    </div>
-  )
+    )
 }
 
 export default Navbar
